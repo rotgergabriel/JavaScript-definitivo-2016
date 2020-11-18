f00fe945f016088764769c1bbfb04ae317798f08
Gabriel <rotgergabriel@gmail.com>
2020-11-18 15:26:36 -0300

    Temporaly Save

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-alert('it´s ok')
+var page =require('page');
+
+var main = document.getElementById('main-container');
+
+page('/', function() {
+    main.innerHTML = 'Home';
+})
+page('/signup', function(ctx, next) {
+    main.innerHTML = 'Signup';
+})
