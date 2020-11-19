60f7ec0efbb57db0be65151b63a092c4cda05759
Gabriel <rotgergabriel@gmail.com>
2020-11-18 23:54:34 -0300

    save day 18/11/20

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ var rename = require('gulp-rename');
 var babel = require('babelify');
 browserify = require('browserify');
 var source = require('vinyl-source-stream');
+var watchify = require('watchify')
 
 
 gulp.task('styles', function () {
@@ -18,14 +19,35 @@ gulp.task('assets', function() {
         .pipe(gulp.dest('public'));
 })
 
-gulp.task('scripts', function () {
-    browserify('./src/index.js')
+function compile(watch) {
+    var bundle = watchify(browserify('./src/index.js'));
+
+    function rebundle() {
+        bundle
         .transform(babel, { presets: ['env'] })
         .bundle()
         .pipe(source('index.js'))
         .pipe(rename('app.js'))
         .pipe(gulp.dest('public')) 
+    }
+
+    if(watch) {
+        bundle.on('update', function() {
+            console.log('--> Bundling...');
+            rebundle();
+        })
+    }
+    rebundle();
+}
+
+gulp.task('build', function() {
+    return compile();
 });
 
-gulp.task('default', gulp.series('styles', 'assets', 'scripts'))
+gulp.task('watch', function() {
+    return compile(true);
+});
+
+
+gulp.task('default', gulp.series('styles', 'assets', 'build'))
 
